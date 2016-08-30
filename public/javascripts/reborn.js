cab1bc7af71cb28d997064131db9081c81cfbe64
rodaz <rodaz@i.ua>
2016-08-30 15:00:44 +0300

    0.1

--- public/javascripts/reborn.js
+++ public/javascripts/reborn.js
@@ -1,66 +1,48 @@
-function load() {
-	$('#loadbtn').hide();
-	$.post(
-	  "/rest/load",
-	  {},
-  	  function (data) {console.log(data); create(data);}
-  	);
-}
-var arr = [
-			['АХО', '75/01,16М', new Date('2015-11-25'), new Date('2016-01-01'), 'ТОВ Днепруниверсал', 'вывоз бытовых отходов', 'счет', 'UAH', new Date('2016-11-31'), '35809022', 'Баранов А.С.', false],
-			['ОГТ', '', new Date('2016-01-18'), new Date('2016-11-1'), 'ООО МВМ Груп ЛТД', 'покупка интструмента', 'спец', 'UAH', new Date('2016-11-31'), '39470596', 'Яковлева Н.И.', false],
-			['ОГМ', '242', new Date('2016-01-18'), new Date('2015-11-18'), 'ООО Промтехаудит', 'работы по монтажу и демонтажу крана мостового', '15928.46', 'UAH', new Date('2016-2-31'), '36840681', 'Молдован А.В.', false],
-			['ОИТ', '473042', new Date('2016-01-25'), new Date('2016-1-11'), 'ПАТ КБ Приватбанк', 'денежные средства', '70000.00', 'USD', new Date('2016-11-31'), '35809022', 'Баранов А.С.', false]
-		];
-function save() {
-	$.post(
-		"/rest/save",
-		{data: arr}
-	);
-}
+$(document).ready(function(){
+	$('#search').on('click', function() {
+		$.post("/rest/search", {
+			year: $('#year').val(),
+			branch: $('#branch').val(),
+			partner: $('#partner').val(),
+			exec: $('#exec').val(),
+			done: $('#done').val()
+		}, function (data) {
+			create(data);
+		});
+	});
 
-function search(){
-
-}
-
-function add(){
-
-}
-
-function create(data) {
-	var content = "<table class='table table-bordered root'>";
-	content += `<tr>
-		<th>Ред.</th>
-		<th>Отдел</th>
-		<th>№ контрагента</th>
-		<th>Дата регистрации</th>
-		<th>Дата заключения</th>
-		<th>Контрагент</th>
-		<th>Предмет договора</th>
-		<th>Сумма с ндс.</th>
-		<th>Валюта</th>
-		<th>Срок действия</th>
-		<th>ЕГРПОУ</th>
-		<th>Отв. исполнитель</th>
-		<th>Утверждение</th>
-	</tr>`;
-	for(i=0; i<data.length; i++){
-		content += '<tr>';
-		content += '<td><a href=\'rest/edit/'+data[i]._id+'\'><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></td>';
-		for(key in data[i]){
-			if (key == '_id' || key == '__v') continue;
-			if (key == 'done') {
-				if (data[i][key] == false) {
-					content += '<td><span class="rem glyphicon glyphicon-remove" aria-hidden="true"></span></td>';
-				} else {
-					content += '<td><span class="rem glyphicon glyphicon-ok" aria-hidden="true"></span></td>';
-				}
-				continue;
-			} 
-			content += '<td>' + data[i][key] + '</td>';
+	function create(data) {
+		var content = "<table class='table table-bordered root'>";
+		content += `<tr>
+			<th>Ред.</th>
+			<th>Отдел</th>
+			<th>№ контрагента</th>
+			<th>Дата регистрации</th>
+			<th>Дата заключения</th>
+			<th>Контрагент</th>
+			<th>Предмет договора</th>
+			<th>Сумма с ндс.</th>
+			<th>Валюта</th>
+			<th>Срок действия</th>
+			<th>ЕГРПОУ</th>
+			<th>Отв. исполнитель</th>
+			<th>Примечание</th>
+		</tr>`;
+		for(i=0; i<data.length; i++){
+			content += '<tr>';
+			content += '<td><a href=\'rest/edit/'+data[i]._id+'\'><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></td>';
+			for(key in data[i]){
+				if (key == '_id' || key == '__v') continue;
+				if (key == 'date_reg' || key == 'date_close' || key == 'validity') {
+					content += '<td>' + moment(data[i][key]).format('L') + '</td>';
+					continue;
+				} 
+				content += '<td>' + data[i][key] + '</td>';
+			}
+			content += '</tr>';
 		}
-		content += '</tr>';
+		content += "</table>";
+		$('#core').empty();
+		$('#core').append(content);
 	}
-	content += "</table>";
-	$('#core').append(content);
-}
+});
--- routes/rest.js
+++ routes/rest.js
@@ -1,159 +1,124 @@
 var express = require('express');
 var router = express.Router();
 var Pact = require('../models/pact');
+var moment = require('moment');
 
-var data;
-
-router.post('/load', function (req, res) {
-	console.log('true');
-	Pact.find(function(err, pacts) {
-		if(err) return console.log(err);
-		console.log(pacts);
-		data = pacts;
-		res.send(pacts);
-	});
-});
-
-router.post('/save', function (req, res) {
-	req.body.data.forEach(function(row) {
-		var pact = new Pact({
-			branch: row[0]?row[0]:'',
-			partner_id: row[1]?row[1]:'',
-			date_reg: row[2]?row[2]:new Date(),
-			date_close: row[3]?row[3]:new Date(),
-			partner: row[4]?row[4]:'',
-			subject: row[5]?row[5]:'',
-			amount: row[6]?row[6]:'',
-			currency: row[7]?row[7]:'',
-			validity: row[8]?row[8]:new Date(),
-			key: row[9]?row[9]:'',
-			exec: row[10]?row[10]:'',
-			done: row[11]?row[11]:false
-		});
-		pact.save(function (err) {
-			if (err) console.log(err);
-			console.log('Done '+pact);
-		});
-	});
-	res.end();
+router.get('/add', function(req, res) {
+    var deps=[], parts=[], exes=[];
+    Pact.find(function(err, pacts) {
+        if (err) console.log(err)
+        else {
+            pacts.forEach(function(val) {
+                deps.push(val.branch);
+                parts.push(val.partner);
+                exes.push(val.exec);
+            });
+            
+            res.render('add', {  
+                deps: unique(deps),
+                parts: unique(parts),
+                exes: unique(exes)
+            });
+        }
+    });
 });
 
 router.get('/edit/:id', function(req, res) {
-	var deps=[], parts=[], exes=[];
-	Pact.find(function(err, pacts){
-		if(err) console.log(err)
-		else {
-			pacts.forEach(function(val) {
-				deps.push(val.branch);
-				parts.push(val.partner);
-				exes.push(val.exec);
-			});
+    var deps=[], parts=[], exes=[];
+    Pact.find(function(err, pacts) {
+        if (err) console.log(err)
+        else {
+            pacts.forEach(function(val) {
+                deps.push(val.branch);
+                parts.push(val.partner);
+                exes.push(val.exec);
+            });
 
-			Pact.findOne({_id: req.params.id}, function(err, fp) {
-				if(err) return console.log(err);
-				console.log(fp);
-				res.render('edit', {
-					br: fp.branch,
-					id: fp.partner_id,
-					re: fp.date_reg,
-					cl: fp.date_close,
-					pa: fp.partner,
-					th: fp.subject,
-					am: fp.amount,
-					cu: fp.currency,
-					da: fp.date,
-					ke: fp.key,
-					ex: fp.exec,
-					d: fp.done,
-					deps: unique(deps),
-					parts: unique(parts),
-					exes: unique(exes)
-				});
-			});
-		}
-	});
+            Pact.findOne({_id: req.params.id}, function(err, fp) {
+                if (err) return console.log(err);
+                res.render('edit', {
+                    _i: fp._id,
+                    br: fp.branch,
+                    id: fp.partner_id,
+                    re: moment(fp.date_reg).format('YYYY-MM-DD'),
+                    cl: moment(fp.date_close).format('YYYY-MM-DD'),
+                    pa: fp.partner,
+                    th: fp.subject,
+                    am: fp.amount,
+                    cu: fp.currency,
+                    da: moment(fp.date).format('YYYY-MM-DD'),
+                    ke: fp.key,
+                    ex: fp.exec,
+                    d: fp.done,
+                    deps: unique(deps),
+                    parts: unique(parts),
+                    exes: unique(exes)
+                });
+            });
+        }
+    });
 
-});
-
-router.post('/saveOne', function(req, res) {
-	var row = req.body;
-	console.log(row);
-
-	var pact = new Pact({
-			branch: row[1]?row[1]:'',
-			partner_id: row[2]?row[2]:'',
-			date_reg: row[3]?row[3]:new Date(),
-			date_close: row[4]?row[4]:new Date(),
-			partner: row[5]?row[5]:'',
-			subject: row[6]?row[6]:'',
-			amount: row[7]?row[7]:'',
-			currency: row[8]?row[8]:'',
-			validity: row[9]?row[9]:new Date(),
-			key: row[10]?row[10]:'',
-			exec: row[11]?row[11]:'',
-			done: row[12]?row[12]:false
-		});
-	Pact.findOneAndUpdate({_id: row._id}, pact, function(err){
-		if(err) console.log(err);
-	});
-	res.end();
-});
-
-router.get('/add', function(req, res){
-	var deps=[], parts=[], exes=[];
-	Pact.find(function(err, pacts){
-		if(err) console.log(err)
-		else {
-			pacts.forEach(function(val) {
-				deps.push(val.branch);
-				parts.push(val.partner);
-				exes.push(val.exec);
-			});
-
-			res.render('add', { 
-				title: 'Express', 
-				deps: unique(deps),
-				parts: unique(parts),
-				exes: unique(exes)
-			});
-		}
-	});
-});
-
-router.post('/search', function(req, res) {
-	var params = {};
-	if (req.body.branch) {params.branch = req.body.branch}
-	if (req.body.partner) {params.partner = req.body.partner}
-	if (req.body.exec) {params.exec = req.body.exec}
-	if (req.body.done) {params.done = req.body.done}
-	console.log(req.body);
-	Pact.find(params, function(err, search_data) {
-		if(err) console.log(err);
-		console.log(search_data);
-	});
-	res.end();
 });
 
 router.post('/add', function(req, res) {
-	var row = req.body;
-	var pact = new Pact({
-		branch: row.branch?row.branch:'',
-		partner_id: row.partner_id?row.partner_id:'',
-		date_reg: row.reg?row.reg:new Date(),
-		date_close: row.close?row.close:new Date(),
-		partner: row.partner?row.partner:'',
-		subject: row.theme?row.theme:'',
-		amount: row.amount?row.amount:'',
-		currency: row.curr?row.curr:'',
-		validity: row.date?row.date:new Date(),
-		key: row.key?row.key:'',
-		exec: row.exec?row.exec:'',
-		done: row.done?row.done:false
-	});
-	pact.save(function (err) {
-		if (err) console.log(err);
-		console.log('Done '+pact);
-	});
+    var row = req.body;
+    var pact = new Pact({
+        branch: row.branch?row.branch:'',
+        partner_id: row.partner_id?row.partner_id:'',
+        date_reg: row.reg?row.reg:moment(),
+        date_close: row.close?row.close:moment(),
+        partner: row.partner?row.partner:'',
+        subject: row.theme?row.theme:'',
+        amount: row.amount?row.amount:'',
+        currency: row.curr?row.curr:'',
+        validity: row.date?row.date:moment(),
+        key: row.key?row.key:'',
+        exec: row.exec?row.exec:'',
+        done: row.done?row.done:''
+    });
+    pact.save(function (err) {
+        if (err) console.log(err);
+    });
+    res.redirect('/');
+});
+
+router.post('/save', function(req, res) {
+    var row = req.body;
+    var pact = {
+            branch: row.branch?row.branch:'',
+            partner_id: row.partner_id?row.partner_id:'',
+            date_reg: row.reg?row.reg:moment(),
+            date_close: row.close?row.close:moment(),
+            partner: row.partner?row.partner:'',
+            subject: row.theme?row.theme:'',
+            amount: row.amount?row.amount:'',
+            currency: row.curr?row.curr:'',
+            validity: row.date?row.date:moment(),
+            key: row.key?row.key:'',
+            exec: row.exec?row.exec:'',
+            done: row.done?row.done:''
+        };
+    Pact.findByIdAndUpdate(row._id, pact, function(err){
+        if(err) console.log(err);
+    });
+    res.redirect('/');
+});
+
+router.post('/search', function(req, res) {
+    var params = {};
+    if (req.body.year)
+        params.date_reg = {
+            $gt:moment(req.body.year+'-01-01'),
+            $lt:moment(req.body.year+'-12-31')
+        };
+    if (req.body.branch) params.branch = req.body.branch;
+    if (req.body.partner) params.partner = req.body.partner;
+    if (req.body.exec) params.exec = req.body.exec;
+    if (req.body.done) params.done = req.body.done;
+    Pact.find(params, function(err, search_data) {
+        if(err) console.log(err);
+        res.send(search_data);
+    });
 });
 
 function unique(arr) {
